a0ed45d5271d6f188c7087b220ebae3dac8b4d80
Artur Czepiel <czepiel.artur@gmail.com>
2022-12-03 12:06:39 +0100

    2022 03 cpp

---
+++ 2022/03/02_cpp.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int score(char x) {
+    if('a' <= x && x <= 'z') {
+        return x - 'a' + 1;
+    } else {
+        return 26 + (x - 'A') + 1;
+    }
+}
+
+int main() {
+    ifstream input;
+    input.open("input");
+
+    string line;
+
+    int ans = 0;
+
+    while(input) {
+        getline(input, line);
+        if(line == "") {
+            break;
+        }
+
+        int half = line.length() / 2;
+        auto a = line.substr(0, half);
+        auto b = line.substr(half);
+        vector<char> av (a.begin(), a.end());
+        vector<char> bv (b.begin(), b.end());
+
+        // Unfortunately no set.intersection(a, b) for unordered sets in cpp :(
+        vector<char> inter = {};
+        for(auto a: av) {
+            for(auto b: bv) {
+                if(a == b) {
+                    inter.push_back(a);
+                    break;
+                }
+            }
+        }
+
+        ans += score(inter[0]);
+    }
+    cout << "Part1 " << ans << endl;
+
+    input.clear();
+    input.seekg(0);
+
+    ans = 0;
+    vector<vector<string>> batches;
+    vector<string> batch;
+    int i = 0;
+    while(input) {
+        getline(input, line);
+        if(line == "") {
+            break;
+        }
+
+        if(i > 0 && i % 3 == 0) {
+            // Can't do the intersection here because it would miss the last batch
+            batches.push_back(batch);
+            batch = {};
+        }
+        batch.push_back(line);
+        i++;
+    }
+    batches.push_back(batch);
+
+    for(auto batch: batches) {
+        bool found = false;
+        for(auto x: batch[0]) {
+            if(found) break;
+            for(auto y: batch[1]) {
+                if(found) break;
+                for(auto z: batch[2]) {
+                    if(found) break;
+                    if(x == y && y == z) {
+                        ans += score(x);
+                        found = true;
+                    }
+                }
+            }
+        }
+    }
+
+    cout << "Part2 " << ans << endl;
+
+}
