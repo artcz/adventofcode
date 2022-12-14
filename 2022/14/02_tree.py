1525a55e41e0fbf44777b6987014312fa3449351
Artur Czepiel <czepiel.artur@gmail.com>
2022-12-14 11:32:54 +0100

    2022 14 oneliner draft #1

---
+++ 2022/14/02_tree.py
@@ -0,0 +1,42 @@
+(                                                          lambda
+                                                          s,ft, i:
+                                                         (getattr(s,
+                                                        "setrecursio"
+                                                       "nlimit")(2500)
+                                                      ,print( [ len([ v
+                                                     for k,v in getattr(
+                                                    ft, "reduce")(lambda pr,
+                                                  b:{**pr,"G":{   **pr["G"],(
+                                                 lambda a:lambda v:  a(a, v ))
+                                                (lambda s,arg: ((((arg[0],arg[1
+                                               ])if(arg!=(500,0)) else None ) if
+                                              pr["p2"]else((arg[0],arg[1])if arg[
+                                             1]<pr["maxy"] else None)) if((arg[0],
+                                            arg[1]+1)in pr['G']and(arg[0]-1,arg[1]+
+                                           1)in pr['G']and(arg[0]+1,arg[1]+1)in pr[''
+                                          'G'])or arg[1]>=pr["maxy"]+1 else(s(s,(arg[0
+                                         ]+0,arg[1]+1))if(arg[0]+0,arg[1]+1)not in pr[""
+                                        'G']else(s(s,(arg[0 and "ooooooooooooooooooooooo"
+                                       ##############i 0]-1,arg[1]+1))if(arg[0]-1,[1]1)not
+                                                    ]-1,arg[1]+1))if(arg[0]-1,arg[1]+1) not
+                                                   in pr['G']else(s(s,(arg[0]+1,arg[1]+1))if(
+                                                  arg[0]+1,arg[1]+1)not in pr['G'] else (None,
+                                               print(arg)))))))((500,0)) if None not in pr["G"]
+                                              else None:"o"},}, ###############################
+                                            range(50000),ft.reduce(### AoC 2022 Day 14 ########
+                                           lambda a, b:{**a, "maxy":
+                                         max(k[1]for k in a["G"].keys(
+                                        ))},range(1), { "G":{(x, y):"#"
+                                      for item in[[tuple(map(int,pos.split(
+                                     ',')))for pos in item] for item in[line.
+                                    strip()     .split('->') for line in data.
+                                  splitlines()]]for m,n in zip(item,item[1:])for
+                                 x in range(min(m[0],n[0]),max(m[0], #########################
+                              n[0])+1)for y in range(min(m[1],n[1]),
+                             max(m[1],n[1])+1)},"p2":p2,})) ["G"].
+                            ####################################### for(data,p2)in((i,False),(i,True))])
+                                                                    items()if v=="o" and k is not None])+p2
+                                                                   for(data,p2)in((i,False),(i,True))])))(
+                                                                 __import__("sys"), __import__("functools"),
+                                                               open(0).read().strip()) #########################
+                      ################################################################################################
